42f8d27376c7c8f3e30e7453ee5e466cbc70429e
DeepakshiPurushotham <deepakshipurushotham@gmail.com>
2022-10-10 10:17:33 +0530

    init

--- securitygroup.tf
+++ securitygroup.tf
@@ -7,6 +7,12 @@ resource "aws_security_group" "web-sg" {
         cidr_blocks = [ "0.0.0.0/0" ]
     }
     ingress {
+  from_port = 0
+   to_port = 0
+   protocol = -1
+   cidr_blocks = ["172.16.4.0/22"]
+  }
+    ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
